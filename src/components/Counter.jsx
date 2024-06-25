ff3170f96a42b480b4b98273f4c23723b8c467ff
dasom222g <dasom228@gmail.com>
2024-06-25 15:55:45 +0900

    Add state, props

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
+import CounterButton from "./CounterButton";
 
 const Counter = () => {
   // logic
-  let counter = 0;
-
   const [counter2, setCounter2] = useState(0);
+  const [step, setStep] = useState(1);
 
   const increase = () => {
-    counter += 1;
-    // console.log("🚀 counter:", counter);
     setCounter2(counter2 + 1);
-    // console.log("🚀 ~ increase");
+  };
+
+  const handleClick = (data) => {
+    console.log("data", data);
+    data === "increase"
+      ? setCounter2(counter2 + step)
+      : setCounter2(counter2 - step);
   };
 
   useEffect(() => {
@@ -23,7 +27,9 @@ const Counter = () => {
       <div>Counter</div>
       <div>{counter2}</div>
       <div className="flex gap-1 justify-center">
-        <button
+        <CounterButton step={step} type="increase" onClick={handleClick} />
+        <CounterButton step={step} type="decrease" onClick={handleClick} />
+        {/* <button
           type="button"
           className="bg-blue-800 text-white px-1"
           onClick={increase}
@@ -32,7 +38,7 @@ const Counter = () => {
         </button>
         <button type="button" className="bg-blue-800 text-white px-1">
           -1
-        </button>
+        </button> */}
       </div>
     </div>
   );
